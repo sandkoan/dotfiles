26f83ba359bf61747061fb8bb06c3f39c2de9381
Govind Gnanakumar <govind.gnanakumar@outlook.com>
2020-07-21 15:52:34 -0700

    Changed functions

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,6 +1,7 @@
 alias rm='rm -I'
 alias vim='nvim'
 
+# exa aliases
 # if type exa 2>/dev/null; then
 # alias ls='exa'
 # alias l='exa -l --all --group-directories-first --git'
@@ -9,18 +10,25 @@ alias vim='nvim'
 # alias llt='exa -lT --git-ignore --level=2 --group-directories-first'
 # alias lT='exa -T --git-ignore --level=4 --group-directories-first'
 # else
-alias l='ls -lahCF'
-alias ll='ls -alF'
-alias la='ls -A'
+# alias l='ls -lahCF'
+# alias ll='ls -alF'
+# alias la='ls -A'
 # fi
 
+# ls aliases
+# alias l='ls -lahCF'
+# alias ll='ls -alF'
+# alias la='ls -A'
+
+# lsd aliases
+alias ls='lsd'
+alias l='ls -l'
+alias la='ls -a'
+alias lla='ls -la'
+alias lt='ls --tree'
+
+# TODO if you run clear, every 10th time run asciiquarium or sl steam locomotive
 alias cls='clear'
 
-# if you run clear, every 10th time run asciiquarium
-# or sl steam locomotive
 
-# function cls() {
-    # sl;
-    # clear;
-# }
 alias config='/usr/bin/git --git-dir=/home/govindg/.cfg/ --work-tree=/home/govindg'
