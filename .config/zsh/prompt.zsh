973bf4b70df85e72e39b55b118b8b5f8cae1dc08
Govind Gnanakumar <govind.gnanakumar@outlook.com>
2020-08-11 14:38:19 -0700

    Finally got zsh prompt **and** cursor to change depending on vi mode, removed some seemingly unneeded stuff in spectrwm config

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -1,72 +1,48 @@
 if [[ ! -a ~/.config/zsh/zsh-async ]]
 then
-  git clone https://github.com/mafredri/zsh-async.git ~/.config/zsh/zsh-async 2> /dev/null
+    git clone https://github.com/mafredri/zsh-async.git ~/.config/zsh/zsh-async 2> /dev/null
 fi
 source ~/.config/zsh/zsh-async
 
 # enable substitution for prompt
 setopt prompt_subst
 
+# change cursor and prompt depending on vi mode
 vim_ins_mode="❯"
 vim_cmd_mode="❮"
 vim_mode=$vim_ins_mode
 
-# Change cursor shape for different vi modes.
 function zle-keymap-select {
-  if [[ ${KEYMAP} == vicmd ]] || [[ $1 = 'block' ]]
-  then
-    echo -ne '\e[1 q'
-    vim_mode=$vim_cmd_mode
-  elif [[ ${KEYMAP} == main ]] || [[ ${KEYMAP} == viins ]] || [[ ${KEYMAP} = '' ]] || [[ $1 = 'beam' ]]
-  then
-    echo -ne '\e[5 q'
-    vim_mode=$vim_ins_mode
-  fi
+    if [ $KEYMAP = vicmd ]
+    then
+        vim_mode=$vim_cmd_mode
+        echo -ne '\e[1 q'
+    else
+        vim_mode=$vim_ins_mode
+        echo -ne '\e[5 q'
+    fi
+    zle reset-prompt
 }
 zle -N zle-keymap-select
-zle-line-init() {
-  zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
-  echo -ne "\e[5 q"
-  vim_mode=$vim_ins_mode
+
+function zle-line-finish {
+    vim_mode=$vim_ins_mode
 }
-zle -N zle-line-init
-echo -ne '\e[5 q' # Use beam shape cursor on startup.
-preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.
-
-# vim_ins_mode="❯"
-# vim_cmd_mode="❮"
-# vim_mode=$vim_ins_mode
-
-# function zle-keymap-select {
-# if [ $KEYMAP = vicmd ]
-# then
-# vim_mode=$vim_cmd_mode
-# else
-# vim_mode=$vim_ins_mode
-# fi
-# zle reset-prompt
-# }
-# zle -N zle-keymap-select
-
-# function zle-line-finish {
-# vim_mode=$vim_ins_mode
-# }
-# zle -N zle-line-finish
+zle -N zle-line-finish
 
 # Fix a bug when you C-c in CMD mode and you'd be prompted with CMD mode indicator, while in fact you would be in INS mode
 # Fixed by catching SIGINT (C-c), set vim_mode to INS and then repropagate the SIGINT, so if anything else depends on it, we will not break it
-# Thanks Ron! (see comments)
 function TRAPINT() {
-  vim_mode=$vim_ins_mode
-  return $(( 128 + $1 ))
+    vim_mode=$vim_ins_mode
+    return $(( 128 + $1 ))
 } 
 
 exit_status() {
-  # 256 colors
-  echo '%(?.%F{76}${vim_mode}%f %{$reset_color%}.%F{196}${vim_mode}%f %{$reset_color%})'
+    # 256 colors
+    echo '%(?.%F{76}${vim_mode}%f %{$reset_color%}.%F{196}${vim_mode}%f %{$reset_color%})'
 }
 exit_status_root(){
-  echo '%(?.%{$fg[green]%}# %{$reset_color%}.%{$fg[red]%}# %{$reset_color%})'
+    echo '%(?.%{$fg[green]%}# %{$reset_color%}.%{$fg[red]%}# %{$reset_color%})'
 }
 
 PROMPT="%(!.%{$fg[red]%}%1~%{$reset_color%} "$(exit_status_root)" .%F{32}%(4~|%-2~/.../%1~|%~)%f%{$reset_color%} "$(exit_status)""
@@ -83,42 +59,42 @@ GIT_PROMPT_MODIFIED="%{$fg_bold[yellow]%}●%{$reset_color%}"     # yellow circl
 GIT_PROMPT_STAGED="%{$fg_bold[green]%}●%{$reset_color%}"        # green circle   - staged changes present = ready for "git push"
 
 parse_git_branch() {
-  # Show Git branch/tag, or name-rev if on detached head
-  ( git symbolic-ref -q HEAD || git name-rev --name-only --no-undefined --always HEAD ) 2> /dev/null
+    # Show Git branch/tag, or name-rev if on detached head
+    ( git symbolic-ref -q HEAD || git name-rev --name-only --no-undefined --always HEAD ) 2> /dev/null
 }
 
 parse_git_state() {
-  # Show different symbols as appropriate for various Git repository states
-  # Compose this value via multiple conditional appends.
-  local GIT_STATE=""
-  local NUM_AHEAD="$(git log --oneline @{u}.. 2> /dev/null | wc -l | tr -d ' ')"
-  if [ "$NUM_AHEAD" -gt 0 ]; then
-    GIT_STATE=$GIT_STATE${GIT_PROMPT_AHEAD//NUM/$NUM_AHEAD}
-  fi
-  local NUM_BEHIND="$(git log --oneline ..@{u} 2> /dev/null | wc -l | tr -d ' ')"
-  if [ "$NUM_BEHIND" -gt 0 ]; then
-    GIT_STATE=$GIT_STATE${GIT_PROMPT_BEHIND//NUM/$NUM_BEHIND}
-  fi
-  local GIT_DIR="$(git rev-parse --git-dir 2> /dev/null)"
-  if [ -n $GIT_DIR ] && test -r $GIT_DIR/MERGE_HEAD; then
-    GIT_STATE=$GIT_STATE$GIT_PROMPT_MERGING
-  fi
-  if [[ -n $(git ls-files --other --exclude-standard 2> /dev/null) ]]; then
-    GIT_STATE=$GIT_STATE$GIT_PROMPT_UNTRACKED
-  fi
-  if ! git diff --quiet 2> /dev/null; then
-    GIT_STATE=$GIT_STATE$GIT_PROMPT_MODIFIED
-  fi
-  if ! git diff --cached --quiet 2> /dev/null; then
-    GIT_STATE=$GIT_STATE$GIT_PROMPT_STAGED
-  fi
-  if [[ -n $GIT_STATE ]]; then
-    echo "$GIT_PROMPT_PREFIX$GIT_STATE$GIT_PROMPT_SUFFIX"
-  fi
+    # Show different symbols as appropriate for various Git repository states
+    # Compose this value via multiple conditional appends.
+    local GIT_STATE=""
+    local NUM_AHEAD="$(git log --oneline @{u}.. 2> /dev/null | wc -l | tr -d ' ')"
+    if [ "$NUM_AHEAD" -gt 0 ]; then
+        GIT_STATE=$GIT_STATE${GIT_PROMPT_AHEAD//NUM/$NUM_AHEAD}
+    fi
+    local NUM_BEHIND="$(git log --oneline ..@{u} 2> /dev/null | wc -l | tr -d ' ')"
+    if [ "$NUM_BEHIND" -gt 0 ]; then
+        GIT_STATE=$GIT_STATE${GIT_PROMPT_BEHIND//NUM/$NUM_BEHIND}
+    fi
+    local GIT_DIR="$(git rev-parse --git-dir 2> /dev/null)"
+    if [ -n $GIT_DIR ] && test -r $GIT_DIR/MERGE_HEAD; then
+        GIT_STATE=$GIT_STATE$GIT_PROMPT_MERGING
+    fi
+    if [[ -n $(git ls-files --other --exclude-standard 2> /dev/null) ]]; then
+        GIT_STATE=$GIT_STATE$GIT_PROMPT_UNTRACKED
+    fi
+    if ! git diff --quiet 2> /dev/null; then
+        GIT_STATE=$GIT_STATE$GIT_PROMPT_MODIFIED
+    fi
+    if ! git diff --cached --quiet 2> /dev/null; then
+        GIT_STATE=$GIT_STATE$GIT_PROMPT_STAGED
+    fi
+    if [[ -n $GIT_STATE ]]; then
+        echo "$GIT_PROMPT_PREFIX$GIT_STATE$GIT_PROMPT_SUFFIX"
+    fi
 }
 
 git_prompt_string() {
-  local git_where="$(parse_git_branch)"
+    local git_where="$(parse_git_branch)"
 
   # If inside a Git repository, print its branch and state
   [ -n "$git_where" ] && echo "$GIT_PROMPT_SYMBOL$(parse_git_state)$GIT_PROMPT_PREFIX%{$fg_bold[blue]%}${git_where#(refs/heads/|tags/)}$GIT_PROMPT_SUFFIX"
