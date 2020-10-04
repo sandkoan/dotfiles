6b448c46c428b0fda9b20167b1b9c27289cd0541
Govind Gnanakumar <govind.gnanakumar@outlook.com>
2020-10-03 22:22:50 -0700

    Added a cdr function

---
+++ .config/zsh/dirs.zsh
@@ -0,0 +1,26 @@
+# directory functions and aliases
+
+# cdr
+autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
+add-zsh-hook chpwd chpwd_recent_dirs
+
+# autocd by typing directory name
+setopt auto_cd
+
+# push old dir onto stack
+setopt auto_pushd
+setopt pushdminus
+
+alias ~="cd ~"
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias 1='cd -'
+alias 2='cd -2'
+alias 3='cd -3'
+alias 4='cd -4'
+alias 5='cd -5'
+alias 6='cd -6'
+alias 7='cd -7'
+alias 8='cd -8'
