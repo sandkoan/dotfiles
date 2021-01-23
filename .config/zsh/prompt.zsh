fd3c1c06d2dc126cc3a5ed2cd770ca7f9c76bd0e
Govind Gnanakumar <47906753+sandkoan@users.noreply.github.com>
2021-01-22 23:50:18 -0800

    Update prompt.zsh

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -3,7 +3,7 @@ setopt transient_rprompt
 
 # Echoes a username/host string when connected over SSH (empty otherwise)
 ssh_info() {
-  [[ "$SSH_CONNECTION" != '' ]] && echo "%(!.%{$fg[red]%}.%{$fg[yellow]%})%n%{$reset_color%}@%{$fg[green]%}%m%{$reset_color%}:" || echo ''
+  [[ "$SSH_CONNECTION" != '' ]] && echo "%(!.%{$fg[red]%}.%{$fg[yellow]%})%n%{$reset_color%}@%{$fg[green]%}%m%{$reset_color%}" || echo ''
 }
 
 # Echoes information about Git repository status when inside a Git repository
